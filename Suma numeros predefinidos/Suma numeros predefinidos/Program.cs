db6f182876ae6b39884b6551a5c2864368d236e3
Israel <alainisraelmj@gmail.com>
2024-12-18 17:55:14 -0600

    commit

---
+++ Suma numeros predefinidos/Suma numeros predefinidos/Program.cs
@@ -0,0 +1,10 @@
+﻿
+float a = 3, b = 5, c = 7, d = 9, e = 11, f = 13, operacion;
+
+Console.WriteLine("Este es el valor de los numeros:\n");
+Console.WriteLine("a = 3\nb = 5\nc = 7\nd = 9\ne = 11\nf = 13");
+Console.WriteLine("\ngLa formula para sumar los numeros sera asi:\n(a + b + c) * (d + e) / f");
+    
+operacion = (a + b + c) * (d + e) / f;
+Console.WriteLine("El resultado es {0}", operacion.ToString("#.##"));
+
