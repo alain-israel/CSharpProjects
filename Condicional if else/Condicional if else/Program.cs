771fc0596a8bef6b377368d929bd62051a6b79d3
Israel <alainisraelmj@gmail.com>
2024-12-18 17:49:59 -0600

    commit

--- Condicional if else/Condicional if else/Program.cs
+++ Condicional if else/Condicional if else/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("**** Acceso Válido ****");
+Console.WriteLine("\nCómo te llamas?: ");
+string name = Console.ReadLine();
+Console.WriteLine("\nBienvenido " + name + ".");
+Console.WriteLine("\nQue edad tienes?");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+
+if (edad > 0 && edad < 18)
+{
+    Console.WriteLine("\n" + name + " eres MENOR de edad");
+}
+else if (edad >= 18 && edad < 35)
+{
+    Console.WriteLine("\n" + name + " eres un ADULTO JOVEN");
+}
+else if (edad >= 35 && edad < 65)
+{
+    Console.WriteLine("\n" + name + " eres un ADULTO");
+}
+else if (edad >= 65 && edad < 130)
+{
+    Console.WriteLine("\n" + name + " eres un ADULTO MAYOR");
+}
+else
+{
+    Console.WriteLine("\n" + name + ", debes ingresar tu edad.");
+}
