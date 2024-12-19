5caaee6688556b2f1635c2970a9b32232ba1eea2
Israel <alainisraelmj@gmail.com>
2024-12-19 03:16:59 -0600

    commit

---
+++ Escribir palabra e invertirla/Escribir palabra e invertirla/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string originalWord;
+string emptyString = "";
+Console.WriteLine("Escribe una palabra: ");
+originalWord = Console.ReadLine();
+
+if (originalWord != null) 
+{
+    for (int i = originalWord.Length - 1; i >= 0; i--)
+    {
+       emptyString += originalWord[i];
+    }
+    Console.WriteLine(emptyString);
+}
+else
+{
+    Console.WriteLine("Debes escribir algo");
+}
