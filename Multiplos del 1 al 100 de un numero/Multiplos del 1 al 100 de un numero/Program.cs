7cb85cde3af4e49985fb1c9a26a6d56fd73a8d67
Israel <alainisraelmj@gmail.com>
2024-12-18 18:33:51 -0600

    commit

---
+++ Multiplos del 1 al 100 de un numero/Multiplos del 1 al 100 de un numero/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Ingresa un numero: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nTu numero es " + numero + ".\n\nAhora veras los primeros 10 multiplos de " + numero + ".\n");
+
+int contador = 1;
+while (contador <= 10)
+{
+    int resultado = numero * contador;
+    Console.WriteLine(resultado);
+    contador++;
+}
+
+
