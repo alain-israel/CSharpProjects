0cb34f63f93f5c7c1c610f6ee07ccea069db3375
Israel <alainisraelmj@gmail.com>
2024-12-19 23:52:06 -0600

    commit 2

--- Condicional if else/Condicional if else/Program.cs
+++ Condicional if else/Condicional if else/Program.cs
@@ -1,29 +1,19 @@
 ﻿
-Console.WriteLine("**** Acceso Válido ****");
-Console.WriteLine("\nCómo te llamas?: ");
-string name = Console.ReadLine();
-Console.WriteLine("\nBienvenido " + name + ".");
-Console.WriteLine("\nQue edad tienes?");
-int edad = Convert.ToInt32(Console.ReadLine());
+string name;
+int edad;
 
+Console.WriteLine("\nCómo te llamas?: ");
+name = Console.ReadLine();
+Console.WriteLine("\nBienvenido " + name + ". \nQue edad tienes?: ");
+edad = Convert.ToInt32(Console.ReadLine());
 
 if (edad > 0 && edad < 18)
-{
     Console.WriteLine("\n" + name + " eres MENOR de edad");
-}
 else if (edad >= 18 && edad < 35)
-{
     Console.WriteLine("\n" + name + " eres un ADULTO JOVEN");
-}
 else if (edad >= 35 && edad < 65)
-{
     Console.WriteLine("\n" + name + " eres un ADULTO");
-}
 else if (edad >= 65 && edad < 130)
-{
     Console.WriteLine("\n" + name + " eres un ADULTO MAYOR");
-}
 else
-{
     Console.WriteLine("\n" + name + ", debes ingresar tu edad.");
-}
