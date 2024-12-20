980727b9133c8bfe2733c1d8adf0d5f7b576747e
Israel <alainisraelmj@gmail.com>
2024-12-19 23:33:12 -0600

    commit

---
+++ Numero par  o impar/Numero par  o impar/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int numero;
+
+try
+{
+    Console.Write("Escribe un numero: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    if ((numero % 2) == 0)
+        Console.Write("Escribiste un numero par");
+    else
+        Console.Write("Escribiste un numero impar");
+
+}
+catch
+{
+    Console.Write("\nDebes escribir un número.");
+}
